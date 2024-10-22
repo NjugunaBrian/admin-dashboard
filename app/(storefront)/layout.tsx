4b247098ddf64f4799aa0156d2973a2f68c29e04
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-10-22 20:22:34 +0300

    feat: store pages initialization

---
+++ app/(storefront)/layout.tsx
@@ -0,0 +1,15 @@
+import SiteHeader from '@/components/shells/site-header'
+import React from 'react'
+
+const layout = ({children}: Readonly<{children: React.ReactNode}>) => {
+  return (
+    <div>
+        <SiteHeader>
+            placeholder
+        </SiteHeader>
+        {children}
+    </div>
+  )
+}
+
+export default layout
